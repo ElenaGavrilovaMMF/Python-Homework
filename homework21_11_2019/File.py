80674ab3a694c1294bea7a7187527bdbe8022938
ElenaGavrilovaMMF <43333754+ElenaGavrilovaMMF@users.noreply.github.com>
2019-11-21 19:52:10 +0300

    Update File.py
    Deleted mass

--- homework21_11_2019/File.py
+++ homework21_11_2019/File.py
@@ -2,7 +2,6 @@
 
 f1 = open('F1.txt', 'r')
 f2 = open('F2.txt', 'w')
-massWords = []
 maxWord = ""
 for line in f1:
     if ' ' not in line:
@@ -10,7 +9,6 @@ for line in f1:
         newLine = line.replace("\n", "")
         if len(newLine) > len(maxWord):
             maxWord = newLine
-        massWords.append(newLine)
 f1.close()
 f2.close()
 print(maxWord)
