065ae0c74141fa5078f0b6377e113912ad84018a
ElenaGavrilovaMMF <43333754+ElenaGavrilovaMMF@users.noreply.github.com>
2019-11-21 14:57:07 +0000

    homework 4

---
+++ homework21_11_2019/File.py
@@ -0,0 +1,12 @@
+f1 = open('F1.txt', 'r')
+f2 = open('F2.txt', 'w')
+massWords = []
+maxWord = ""
+for line in f1:
+    if ' ' not in line:
+        f2.write(line)
+        newLine = line.replace("\n", "")
+        if len(newLine) > len(maxWord):
+            maxWord = newLine
+        massWords.append(newLine)
+print(maxWord)
